1bf9fb846398afadb6704dc902aa62bbdd1a52b4
renatagabrielly <132918391+renatagabrielly@users.noreply.github.com>
2025-06-09 09:33:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -47,24 +47,24 @@ const enunciado = [
         alternativas: [
             {
                 texto: "o avanço da inteligência artificial para facilitar a vida das pessoas.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                afirmacao: "voce estara aberto as inovacoes tecbologicas, aproveitando os avanços da inteligencia artificial para facilitar a rotina."
             },
             {
                 texto: "a criação de novas tecnologias sustentáveis que ajudem o meio ambiente.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                afirmacao: "voce vai se interresar por tecnologias sustentaveis, contribuindo para um futuro mais equilibrado e conciente com o meio ambiente"
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "quando pensa no seu futuro pessoal, o que mais te inspira? ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "viajar, conhecer culturas novas e viver experienciencias diferentes.",
+                afirmacao: "seu futuro sera replete de aventuras e novas experiencias, conhecendo diferentes culturas e lugares pelo mundo."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "construir uma vida estavel, com casa própria, família e rotina tranquila.",
+                afirmacao: "voce vai construir umaa vida estavel e tranquila, valorizando a segurança, a familia e o aconchego do lar  . "
             }
         ]
     },
