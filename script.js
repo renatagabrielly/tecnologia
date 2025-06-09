3a639c73accf8191c215f4c1882eacca82c87778
renatagabrielly <132918391+renatagabrielly@users.noreply.github.com>
2025-06-09 08:46:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -30,15 +30,15 @@ const enunciado = [
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "sobre saùde mental, o que você considera mais importante para seu futuro",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "ter uma tempo para cuidar de si mesmo(a) e mmanter o equilíbrio emocional.",
+                afirmacao: "você dará prioridade ao autocuidado e ao equilíbrio emocional, reconhecimento a importância de cuidar da mente."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "aprender a lidar com desafio e se tornar uma. pessoa mais resiliênte,"
+                afirmacao: "Você vai desenvolver resiliênci, enfrentando os desafios com força e aprendendo com experiências da vida."
             }
         ]
     },
